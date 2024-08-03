9b6b2315a44953362cee41ef7f18a570227e12bc
shantanudwivedi2502 <shantanudwivediapple2502@gmail.com>
2024-08-04 01:33:08 +0530

    DSA-pattern

---
+++ patter2.cpp
@@ -0,0 +1,264 @@
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"ente rthe value of n";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+       int  j=1;
+        while(j<=n)
+        {
+            cout<<j;
+             j++;
+        }
+        cout<<endl;
+         i++;
+       
+    }
+   
+    
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter the value of n";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=n){
+            
+            
+            cout<<n-j+1;
+              j++;
+            
+            
+        }
+        cout<<endl;
+        i++;
+    }
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+  
+    cout<<"enter the value n";
+    cin>>n;
+    int i=1;
+     int count =1;
+    while(i<=n)
+    {
+        int j=1;
+         
+        while(j<=n)
+        {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value n ";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            cout<<"*";
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+    }
+}*/
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the value of n";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+       int j=1, value=i;
+        while(j<=i)
+        {
+            cout<<value;
+            value++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n ";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1; 
+        while(j<=i)
+        {
+            cout<<i-j+1;
+            
+            j++;
+
+        }
+        cout<<endl;
+        i++;
+    }
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of n ";
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n)
+        {
+            char ch='A'+i-1;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter the number ";
+    cin>>n;
+
+int i=1;
+while(i<=n)
+{
+    int j=1;
+    while(j<=n)
+    {
+        char ch='A'+j-1;
+        cout<<ch;
+        j++;
+
+    }
+    cout<<endl;
+    i++;
+
+}
+}*/
+/*#include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<<"enter the value of n ";
+    cin>>n;
+    int count=0;
+    int i =1;
+    while(i<=n)
+    {
+        int j =1;
+        while(j<=n)
+        {
+            char ch = 'A'+count;
+            cout<<ch;
+            count++;
+            j++;
+
+
+        }
+        cout<<endl;
+        i++;
+    }
+}*/
+/*#include<iostream>
+using namespace std ;
+int main(){
+    int n ;
+    int value;
+    cout<<"enter the value of n"<<endl;
+    cin>>n;
+    int i =1;
+    while(i<=n)
+    {
+        int j=1; char ch ='A'+value; value=i;
+        while(j<=n)
+        {
+            cout<<ch;
+            value++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+
+}*/
+/*#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"enter the value of n";
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int j=1;
+        while(j<=i)
+        {
+            char ch ='A'+i-1;
+            cout<<ch;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n ;
+    cout<<"enter the value of n";
+    cin>>n;
+    int i =1;
+    while(i<=n)
+    {
+        int j=1; char value=1;
+        while(j<=i)
+        {
+            cout<<value;
+            value++;
+            j++;
+
+
+        }
+        cout<<endl;
+        i++;
+
+    }
+}
